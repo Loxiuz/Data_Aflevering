f00dad41fa5aae3b2d1b27a8b809fc04230b7d6c
Malte <maltebjoerklund@gmail.com>
2023-03-21 22:00:55 +0100

    HTML og Object i Javascript

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 "use strict";
 
-const character = {
-  image: "",
-  name: "",
-  age: 0,
-  gender: "",
-  occupation: "",
-  schoolGrade: 0,
-  religion: "",
-  cathphrase: "",
-  hairColor: "",
-  episodes: "",
-  appearances: 0,
-  firstAppearance: "",
-  voicedBy: "",
+const lincoln = {
+  image:
+    "https://static.wikia.nocookie.net/southpark/images/1/10/Abraham-lincoln.png/revision/latest/scale-to-width-down/182?cb=20170411105546",
+  name: "Abraham Lincoln",
+  age: 56,
+  gender: "Male",
+  occupation: "16th President of the United States",
+  schoolGrade: "None",
+  religion: "None",
+  catchphrase: "None",
+  hairColor: "Grey",
+  episodes: "S11 E14",
+  appearances: 1,
+  firstAppearance: "S11 E14",
+  voicedBy: "Trey Parker",
 };
+
+console.log(lincoln);
