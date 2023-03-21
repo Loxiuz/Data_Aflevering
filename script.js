ce8f158dac3c5bd836a5bf020f910ac77233c63a
Malte <maltebjoerklund@gmail.com>
2023-03-21 15:44:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const character = {
+  image: "",
+  name: "",
+  age: 0,
+  gender: "",
+  occupation: "",
+  schoolGrade: 0,
+  religion: "",
+  cathphrase: "",
+  hairColor: "",
+  episodes: "",
+  appearances: 0,
+  firstAppearance: "",
+  voicedBy: "",
+};
