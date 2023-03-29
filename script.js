ba176bf747a015c832bc4ded3ed5ec0470b26fc3
Malte <maltebjoerklund@gmail.com>
2023-03-29 23:06:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showCharacter(character) {
   const html = /* html */ `
     <div class="grid-item">
       <h2>${character.name}</h2>
-      <image src="${character.image}" />
+      <image src="${character.image}"/>
       <p><span>Age:</span> ${character.age}</p>
       <p><span>Gender:</span> ${character.gender}</p>
       <p><span>Voiced by:</span> ${character.voicedBy}</p>
@@ -29,26 +29,26 @@ function showCharacter(character) {
     console.log(character);
     const dialogHtml = /* html */ `
     <table>
-    <tr>
-    <td>
-      <h2>${character.name}</h2>
-      <p><span>Nickname:</span> ${character.nickname}</p>
-      <image src="${character.image}" />
-    </td>
-    <td  class = "dialog-info">
-      <p><span>Occupation:</span> ${character.occupation}</p>
-      <p><span>Age:</span> ${character.age}</p>
-      <p><span>Voiced by:</span> ${character.voicedBy}</p>
-      <p><span>Gender:</span> ${character.gender}</p>
-      <p><span>Religion:</span> ${character.religion}</p>
-      <p><span>Catchphrase:</span> ${character.catchPhrase}</p>
-      <p><span>Hair color:</span> ${character.hairColor}</p>
-      <p><span>School grade:</span> ${character.schoolGrade}</p>
-      <p><span>Episodes:</span> ${character.episodes}</p>
-      <p><span>Appearances:</span> ${character.appearances}</p>
-      <p><span>First appearance:</span> ${character.firstAppearance}</p>
-    </td>
-    </tr>
+      <tr>
+        <td>
+          <h2>${character.name}</h2>
+          <p><span>Nickname:</span> ${character.nickname}</p>
+          <image src="${character.image}" />
+        </td>
+        <td  class = "dialog-info">
+          <p><span>Occupation:</span> ${character.occupation}</p>
+          <p><span>Age:</span> ${character.age}</p>
+          <p><span>Voiced by:</span> ${character.voicedBy}</p>
+          <p><span>Gender:</span> ${character.gender}</p>
+          <p><span>Religion:</span> ${character.religion}</p>
+          <p><span>Catchphrase:</span> ${character.catchPhrase}</p>
+          <p><span>Hair color:</span> ${character.hairColor}</p>
+          <p><span>School grade:</span> ${character.schoolGrade}</p>
+          <p><span>Episodes:</span> ${character.episodes}</p>
+          <p><span>Appearances:</span> ${character.appearances}</p>
+          <p><span>First appearance:</span> ${character.firstAppearance}</p>
+        </td>
+      </tr>
     </table>
     <form method="dialog">
 		  <button>Close</button>
